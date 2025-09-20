79ab7e9a37a7beaec67cf4eac46bd10de5d10713
Simra-Anjum <simraanjum68@gmail.com>
2025-09-20 19:17:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express  from "express"
+import express from "express"
 import cors from 'cors'
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoute.js"
@@ -7,29 +7,31 @@ import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 import productRouter from "./routes/productRoute.js"
 
-// app config
 const app = express()
 const port = process.env.PORT || 4000;
 
 
-// middlewares
-app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://e-commerce-5-tkdz.onrender.com",  
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
-// db connection
+
+app.use(express.json())
+
+
 connectDB()
 
-// api endpoints
+
 app.use("/api/user", userRouter)
 app.use("/api/product", productRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
-  
+app.use("/api/order", orderRouter)
+
 app.get("/", (req, res) => {
-    res.send("API Working")
-  });
+  res.send("API Working")
+});
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
-
-// mongodb+srv://extrastack:7789s456a@cluster0.hslrmjs.mongodb.net/?
